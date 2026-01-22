a05fb5663b2eddcb7735e587db0ee5fad838f47a
Kvink <114291408+Kvinkgithub@users.noreply.github.com>
2026-01-22 20:03:41 +0300

    Update task.py

--- task.py
+++ task.py
@@ -14,4 +14,58 @@ while a==1:
             list1.append(user_number)
     sort_list1=sorted(list1)
     print(f'Минимальное число оканчивающиеся на 4 это: {sort_list1[:1]}')
+
     a-=1
+
+#Задача 2
+
+# per=()
+# user_str = input('Введите массив из 11 элементов: ')
+# if len(user_str)==11:
+#         for i in user_str:
+#             if i.isalpha():
+#                 per=i.upper()
+#             else:
+#                 per+=i
+#                 print(per,end="")
+#         else:
+#             print("Некорректное число символов!")
+
+#задача 3
+
+# per=""
+# user_str = input('Введите массив из 12 элементов: ')
+# if len(user_str)==12:
+#     for i in user_str:
+#         if i.isdigit():
+#              i="!"
+#              print(i,end="")
+#         else:
+#             print(i,end="")
+
+#задача 9
+
+# count=0
+# text=['book','bro','text','just']
+# for i in text:
+#     if i[:1]=='b':
+#        count+=1
+# print(f'Кол-во слов начинающихся с буквы {count}')
+
+#задача 10
+
+# count=0
+# mass=['r','k','t','z']
+# for i in mass:
+#     if i == 'r' or i == 'k' or i == 't':
+#         count+=1
+# print(f'В массиве количество букв r,k,t: {count}')
+
+#задача 11
+
+# count=0
+# mass=["42",'@','*',';',':']
+# for i in mass:
+#     if i == "*" or i == ":" or i == ';':
+#         count+=1
+# print(f'В массиве количество символов : , ; , * : {count}')
